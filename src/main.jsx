4a60541cbd800a930284066400dee717ffae6bd6
Angelika Friis <angelikafriis@gmail.com>
2025-08-17 13:33:10 +0200

    feat(logging): integrate Sentry for error tracking

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://b968c4dfee79a074b5060d8750f022b8@o4509853236527104.ingest.de.sentry.io/4509853238952016",
+  sendDefaultPii: true
+});
 
 createRoot(document.getElementById('root')).render(
   <App />,
